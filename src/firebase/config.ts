5fac1b17ed2225f06618c8f07c8fae952c2e7c4c
Хвесеня Олександр <drovosek.8215@gmail.com>
2026-02-20 04:26:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-435336616-bec19",
+  "appId": "1:808783788276:web:be4a1a4f6f359c22c786a1",
+  "apiKey": "AIzaSyCX_hchqAF2Zk5vU6jzC3rHCogVMKMo9ic",
+  "authDomain": "studio-435336616-bec19.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "808783788276"
+};
